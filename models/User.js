9210851f3de7ac5a0637a7e2db9f34a944bcae99
Vinay <vinayhegde@outlook.com>
2020-11-20 12:49:47 +0530

    Initial setup and model

---
+++ models/User.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("user", {
+        name: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        mobile: {
+            type: Sequelize.BOOLEAN
+        },
+
+    });
+
+    return User;
+};
