4eaae6344cac692a9cf96e613ec7278b3e17e7dd
Vinay <vinayhegde@outlook.com>
2020-11-23 09:49:57 +0530

    Changes in seeders

--- seeders/20201123041109-default-roles.js
+++ seeders/20201123041109-default-roles.js
@@ -1,24 +1,21 @@
 'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('role', [{
+      role: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      role: 'user',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ]);
   },
-
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: (queryInterface, Sequelize) => {
+    // return queryInterface.bulkDelete('role', { 
+    //   name: 'user'
+    // }, {});
   }
 };
